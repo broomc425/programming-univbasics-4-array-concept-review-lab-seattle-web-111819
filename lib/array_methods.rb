3edc23a304dfd8499c6dde15835467a3fa5b6dcb
Charlie Broom <broomc425@gmail.com>
2019-11-06 22:45:25 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -17,7 +17,8 @@ def find_element_index(array, value_to_find)
     if array[count] == value_to_find
       return count
     end
-    nil
+  end
+  nil
 end
 
 # def find_element_index(array, value_to_find)
